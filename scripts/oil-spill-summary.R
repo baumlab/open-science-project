cafe0ddc6cecd686ed22e4492a4be8b98988c975
James Robinson <jpwrobinson@gmail.com>
2017-01-26 13:39:20 -0800

    link to NOAA incident website

--- scripts/oil-spill-summary.R
+++ scripts/oil-spill-summary.R
@@ -12,44 +12,46 @@ setwd("~/Documents/git-jpwrobinson/open-science-project")
 
 library(scales); library(stringr); theme_set(theme_bw())
 
-oil <- read.table('data/oil-spill-data/noaa-incidents.csv',sep=',',header=T)
-oil<-subset(oil,is.na(oil$lat)==F) # only look at incidents with Lat and Long
-oil <- subset(oil, oil$threat=='Oil') #only choose oil spill incidents
-oil<- subset(oil,is.na(oil$max_ptl_release_gallons)==F) #only look at incidents with information on total number of gallons
+# oil <- read.table('data/oil-spill-data/noaa-incidents.csv',sep=',',header=T)
+# oil<-subset(oil,is.na(oil$lat)==F) # only look at incidents with Lat and Long
+# oil <- subset(oil, oil$threat=='Oil') #only choose oil spill incidents
+# oil<- subset(oil,is.na(oil$max_ptl_release_gallons)==F) #only look at incidents with information on total number of gallons
 
 
-#-----------------------------------STARTING DATE CLEANING------------------------------------------#
-## convert date to Date format 
-## need to add 0s to date + months 
-oil$Date<-as.character(oil$open_date)
+# #-----------------------------------STARTING DATE CLEANING------------------------------------------#
+# ## convert date to Date format 
+# ## need to add 0s to date + months 
+# oil$Date<-as.character(oil$open_date)
 
-### add year, month, day data
-dates<-ldply(strsplit(oil$Date, '/'))
-oil$day<-dates$V2
-oil$month<-dates$V1
-oil$year<-ifelse(dates$V3<20, paste("20", dates$V3, sep=""), paste('19', dates$V3, sep=''))
+# ### add year, month, day data
+# dates<-ldply(strsplit(oil$Date, '/'))
+# oil$day<-dates$V2
+# oil$month<-dates$V1
+# oil$year<-ifelse(dates$V3<20, paste("20", dates$V3, sep=""), paste('19', dates$V3, sep=''))
 
-## add 0s to start of single digit days and months
-oil$month<-ifelse(nchar(oil$month)==1, paste(0, oil$month, sep=""), oil$month)
-oil$day<-ifelse(nchar(oil$day)==1, paste(0, oil$day, sep=""), oil$day)
+# ## add 0s to start of single digit days and months
+# oil$month<-ifelse(nchar(oil$month)==1, paste(0, oil$month, sep=""), oil$month)
+# oil$day<-ifelse(nchar(oil$day)==1, paste(0, oil$day, sep=""), oil$day)
 
-oil$Date<-with(oil, paste(year, month, day, sep='-'))
+# oil$Date<-with(oil, paste(year, month, day, sep='-'))
 
-### change to date format for plotting
-oil$Date<-as.Date(oil$Date, format="%Y-%m-%d")
+# ### change to date format for plotting
+# oil$Date<-as.Date(oil$Date, format="%Y-%m-%d")
 
-## remove surplus date columns
-oil$Day<-NULL; oil$day<-NULL; oil$month<-NULL
+# ## remove surplus date columns
+# oil$Day<-NULL; oil$day<-NULL; oil$month<-NULL
 
-#-----------------------------------FINISHED DATE CLEANING------------------------------------------#
+# #-----------------------------------FINISHED DATE CLEANING------------------------------------------#
 
-neg<-function(x) -x 
-## subset to west coast
-oil.west<-oil[oil$lat>29 & oil$lat<80 &  oil$lon>neg(190) & oil$lon<neg(110),]
+# neg<-function(x) -x 
+# ## subset to west coast
+# oil.west<-oil[oil$lat>29 & oil$lat<80 &  oil$lon>neg(190) & oil$lon<neg(110),]
 
 
-## save Rdata file
-save(oil.west, file='data/oil_west_clean.Rdata')
+# ## save Rdata file
+# save(oil.west, file='data/oil_west_clean.Rdata')
+
+load('data/oil_west_clean.Rdata')
 
 # plot of spill size over time
 pdf(file='figures/oil_spills_overtime.pdf', height=7, width=11)
