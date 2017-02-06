b86176c1021d094afdcac2a0a2aecde79f7ea391
James Robinson <jpwrobinson@gmail.com>
2017-02-06 14:47:02 -0800

    raster plots working but look terrible. need layout

--- scripts/SaU_fish_spatial_grids.R
+++ scripts/SaU_fish_spatial_grids.R
@@ -33,32 +33,45 @@ rast <- raster(ncol = 10, nrow = 10)
 extent(rast) <- extent(pts)
 rasterize(pts, rast, pts$catch, fun = mean)
 
-map("worldHires", xlim=c(-150,-110),ylim=c(29,50), col="gray95", fill=TRUE)
-plot(pts, col=pts$col, cex=0.5, lwd=0.1)
+
+
 
 rast <- raster(ncol = 10, nrow = 10)
 extent(rast) <- extent(pts)
 rasterize(pts, rast, pts$col, fun = mean)
 
 
-plot(1:10, 1:10, col='transparent', axes=FALSE)
-color.legend(2, 3, 9,6,legend=round(range(col_seq[,1]), digits=2), rect.col=col_seq$cols,align="lt", cex=1, gradient="x")
-text(5.5,8, 'Catch (tonnes)', srt=00, cex=1.5)
-
-
-gridded(pts) <- TRUE 
-#convert to raster
-r <- raster(pts)
-#plot
-plot(r)
 
 
 
+
+tot.catch$lon<-ifelse(tot.catch$lon>0, tot.catch$lon-360, tot.catch$lon)
 sites <- SpatialPointsDataFrame(data.frame('lon' = tot.catch[,"lon"], 'lat' = tot.catch[,'lat']), data.frame(catch=tot.catch$sum, col=tot.catch$sum.col))
 proj <- CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0')
 proj4string(sites) <- proj
 
-## set resolution for grid size. reso = 6 is the average reef size in Mellin et al. 2010 (6km)
+## set resolution for grid size and extent 
+
 rast <-raster(xmn=sites@bbox[1,1], xmx=sites@bbox[1,2], ymn=sites@bbox[2,1], ymx=sites@bbox[2,2], 
-              crs = proj, vals=NULL)
-rasterize(sites, rast, pts$sum, fun = mean)
+              crs = proj, res=0.5)
+tt<-rasterize(sites, rast, sites$catch, fun = mean)
+
+
+world360<-readShapePoly("data/Shapes_Merge/Shapes_Merge.shp")
+mat<-matrix(c(1,1,1,1,2,2,3,3), byrow=TRUE, nrow=2)
+layout(mat)
+
+par(mfrow=c(2,2), mar=c(2,2,2,2))
+plot(world360, xlim=c(-150,-110),ylim=c(29,50))
+plot(tt,  lwd=0.5, add=TRUE)
+plot(world360, xlim=c(-150,-110),ylim=c(29,50), add=TRUE)
+
+plot(world360, xlim=c(-150,-110),ylim=c(45,60))
+plot(tt,  lwd=0.5, add=TRUE)
+plot(world360, xlim=c(-150,-110),ylim=c(45,60), add=TRUE)
+
+plot(world360, xlim=c(-200,-130),ylim=c(50, 60))
+plot(tt,  lwd=0.5, add=TRUE)
+plot(world360, xlim=c(-200,-130),ylim=c(50, 60), add=TRUE)
+
+
