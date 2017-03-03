91176aa0a42639c2e52c83917b6307fb9457e71d
erwhite1 <eastonrwhite@gmail.com>
2017-03-03 11:12:11 -0800

    added scripts and figures investigating autocorrelation issues

---
+++ residual_figures/make_residual_plot_vs_time_and_space.R
@@ -0,0 +1,38 @@
+# Created by Easton White
+# Created on 3-Mar-2017
+
+
+# This code runs GAM models following data cleaning from Geoff's script. The script also plots residuals vs time and space to identify spatial or temporal autocorrelation
+
+# Subset models for more recent years and run GAMs for each species
+clams=subset(clams,clams$year>1999)
+snow.crab=subset(snow.crab,snow.crab$year>1999)
+geoduck= subset(geoduck,geoduck$year>1999)
+gam.out.clam<-gam(sum~pdo+year+spill +lat+lon, data=clams, family=Gamma(link=log))
+gam.out.snow<-gam(sum~pdo+year+spill +lat +lon , data=snow.crab, family=Gamma(link=log))
+gam.out.geoduck<-gam(sum~pdo+year+spill+lat+lon, data=geoduck, family=Gamma(link=log)) #
+
+summary(gam.out.clam)
+
+
+clams$residuals = gam.out.clam$residuals
+snow.crab$residuals = gam.out.snow $residuals
+geoduck$residuals = gam.out.geoduck$residuals
+
+
+require(ggplot2)
+ggplot() + geom_point(data=geoduck,aes(x=lon,y=lat,col=log(abs(residuals))),size=4) + facet_wrap(~year)
+
+ggplot() + geom_point(data=geoduck,aes(x=year,y=residuals,col=as.factor(abs(lat*lon))),size=4) 
+
+
+#require(geosphere)
+#clams2005
+#pw=dist(as.data.frame(cbind(clams2005$lon, clams2005$lat)))
+
+#dist(as.matrix(cbind(clams2005$lon, clams2005$lat)),as.matrix(cbind(clams2005$lon, clams2005$lat)), a=6378137, f=1/298.257223563)
+
+#dist(clams$lat,clams$lon)
+
+
+
