4d490466e2d91ca6cc58711e4b8008222eec2f0d
erwhite1 <eastonrwhite@gmail.com>
2017-03-08 16:16:02 -0800

    made changes plots with residuals vs time and space

--- scripts/Spatial_Autocorrelation_models.R
+++ scripts/Spatial_Autocorrelation_models.R
@@ -17,21 +17,21 @@ gam.out.geoduck<-gam(sum~pdo+year+spill +lat+lon, data=geoduck)
 
 
 # Make each lat-lon combo a different study site. Then we can use random effects to account for this like we did at Cocos (this would account for temporal autocorrelation)
-table(clams$lat,clams$lon)
+table(geoduck$lat,geoduck$lon)
 
-all_locations=expand.grid(clams$lat,clams$lon)
+all_locations=expand.grid(geoduck$lat,geoduck$lon)
 all_locations_unique =unique(all_locations)
 
-clams$site_code=NULL
-for (q in 1:nrow(clams)){
-  clams$site_code[q] = which(all_locations_unique$Var1==clams$lat[q] & all_locations_unique$Var2==clams$lon[q])
+geoduck$site_code=NULL
+for (q in 1:nrow(geoduck)){
+  geoduck$site_code[q] = which(all_locations_unique$Var1==geoduck$lat[q] & all_locations_unique$Var2==geoduck$lon[q])
 }
 
 library(MASS)
 library(glmmADMB)
 
-clams$site_code=as.factor(clams$site_code)
-model = glmmadmb(formula=sum~pdo+year+spill+(1|site_code),data=clams,family="poisson",extra.args="-ndi 30000",zeroInflation=FALSE)
+geoduck$site_code=as.factor(geoduck$site_code)
+model = glmmadmb(formula=sum~pdo+year+spill+(1|site_code),data=geoduck,family='Gamma',extra.args="-ndi 30000",zeroInflation=FALSE)
 
 ############################
 
