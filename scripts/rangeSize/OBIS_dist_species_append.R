34b1733a333043f96167c84a69f5cafa11041ce2
JamieMcDevittIrwin <jamie.mcirwin@gmail.com>
2017-10-13 15:46:51 -0700

    calculated eoo from obis data, appended to trade data

--- scripts/rangeSize/OBIS_dist_species_append.R
+++ scripts/rangeSize/OBIS_dist_species_append.R
@@ -4,7 +4,7 @@
 # Created on 11-Oct-2017
 
 theme_set(theme_bw())
-# Purpose: this code reads in OBIS data from Andrew Rhyne, adds to trade.trophic dataset
+# Purpose: this code reads in OBIS data from Andrew Rhyne, find the matching species with trade, calculate EOO
 
 # setwd("~/Desktop/Research/open-science-project")
 # setwd("~/Documents/git-jpwrobinson/open-science-project")
@@ -12,70 +12,68 @@ theme_set(theme_bw())
 
 setwd("~/Documents/git_repos/open-science-project")
 library("ggplot2")
+library(dplyr)
+
 # read full distribution data
 obis<-read.csv(file='data/OBIS_dist_species.csv')
 head(obis)
 tail(obis)
-dim(obis)
+dim(obis) #1393965, 10
+levels(obis$taxa_name) #2509 species 
 # decimal lat and long are both + and - values
 
 
 # Before you can match in the trade data, you need one value for each species from the OBIS data
 # Idea from Jimmy: Find max and min for each sp. Scale lat and lon to mean 0 and sd of 1 across all species. Sum together for each species. Proxy for range size.
 
-# Match OBIS data with trade data
-trade.trophic <- read.csv(file='data/trade_with_trophic_group.csv')
-dim(trade.trophic) #393 8
+
+# match trade data with OBIS
+trade<-read.csv(file='data/trade_taxa_all.csv')
+head(trade)
+str(trade)
+levels(trade$Taxa) #1553 species 
+dim(trade) # 2296, 6 (because there are multiple species in multiple countries )
+
+# subset obis so it only includes species within trade dataset 
+test <- subset(obis,taxa_name %in% trade$Taxa)
+head(test)
+dim(test) #609832 occurences 
+
+# how many species are in the obis dataset from trade? 
+trade$decimalLatitude<-obis$decimalLatitude[match(trade$Taxa, obis$taxa_name)]
+levels(trade$Taxa) # 1553 so all of them!!
 
 
 
 
+# Calculate Extent of Occurence from OBIS data 
+# subset out just decimal and species names 
+test.conr <- dplyr::select(test, one_of(c("decimalLatitude", "decimalLongitude", "taxa_name")))
+head(test.conr)
+dim(test.conr) #609832, 3
+
+library(red)
+head(test.conr)
 
 
+want = unique(test.conr$taxa_name)
+haha = c()
+for(i in 1:length(want)){
+	ha = test.conr[test.conr$taxa_name == want[i], c(2, 1)] # 
+	haha = rbind(haha, data.frame(Species = want[i], EOO = eoo(ha)))
+}
 
+head(haha)
+dim(haha) #1445, 2, so it only calculated EOO for 1445 species 
 
+# merge back to trade data
+trade$EOO<-haha$EOO[match(trade$Taxa, haha$Species)]
+head(trade)
+dim(trade) # 2296, 7
 
+trade.obis <- trade
+trade.obis <- dplyr::select(trade.obis, -X)
+head(trade.obis)
 
-
-# # match in obis
-# trade.trophic$obis_decLatitude<-obis$decimalLatitude[match(trade.trophic$Taxa, obis$scientificName)]
-# head(trade.trophic)
-# trade.trophic$obis_decLongitude<-obis$decimalLongitude[match(trade.trophic$Taxa, obis$scientificName)]
-# head(trade.trophic)
-
-# # rename so we aren't confused 
-# trade.trophic.obis <-trade.trophic
-# head(trade.trophic.obis)
-# dim(trade.trophic.obis)
-
-
-# Try to plot the data onthe map 
-# install.packages("maps")
-library(ggplot2)
-library(maps)
-#Using GGPLOT, plot the Base World Map
-mp <- NULL
-mapWorld <- borders("world", colour="gray50", fill="gray50") # create a layer of borders
-mp <- ggplot() + mapWorld
-mp
-class(mp)
-
-
-
-# plot the bubbles 
-mp <- mp + geom_point(data = obis, aes(x=decimalLongitude, y= decimalLatitude))
-mp
-
-
-
-#need to figure out how to use this data
-
-
-
-
-
-
-
-
-
+save(trade.obis, file="data/trade_with_obis_eoo.Rdata")
 
