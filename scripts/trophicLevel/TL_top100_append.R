3f51a88d27c0bb66dc73f47a34f63d865525defe
James Robinson <jpwrobinson@gmail.com>
2017-11-02 14:33:30 +0400

    tidy folders. started looking at export probability by species traits

--- scripts/trophicLevel/TL_top100_append.R
+++ scripts/trophicLevel/TL_top100_append.R
@@ -32,37 +32,3 @@ dim(trade100.trophic) # only has data for 28 species
 
 
 write.csv(trade100.trophic, file='data/trade100_with_trophic_group.csv')
-
-
-
-## try fish base
-# install.packages("devtools")
-library(devtools)
-devtools::install_github("ropensci/rfishbase")
-library("rfishbase")
-
-trade100$Taxa
-
-# check for misspellings or alternate names 
-x <- synonyms(trade100$Taxa) # 46 warnings saying no results for query
-
-# check for alternate versions of a scientific name and return the names FishBase recognizes as valid 
-fishes <-validate_names(trade100$Taxa, limit=1000) # more than 50 warnings, no match for species
-fishes # how many have NA
-
-fishes<-fishes[!is.na(fishes)]
-fishes # 77 species have names within fishbase 
-
-
-
-# Get trophic level 
-test <-ecology(fishes,fields=c("FoodTroph", "FoodSeTroph", "DietTroph", "DietSeTroph"))
-test # FoodTroph has the most information
-
-fishes.troph<-test[!is.na(test$FoodTroph),]
-fishes.troph # 75 species have trophic level data!!
-
-
-
-
-
